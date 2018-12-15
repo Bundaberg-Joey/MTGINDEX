70664d84362a4d89fea47d83b3fd6ccbc458732a
Bundaberg-Joey <44094475+Bundaberg-Joey@users.noreply.github.com>
2018-12-15 10:02:55 +0000

    Create  version_check.py
    file parses mtgjson online and checks if the currently hosted version matches the most recent version used to build the database.

---
+++ update_check/version_check.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+########################################################################################################################
+
+
+def current_mtgjson_version():
+    """Fetches current version number from mtgjson online and returns value as a string"""
+    page = requests.get('https://mtgjson.com/v4/changelog.html')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    content_soup = soup.find(attrs={'class': 'entry-content'})  # html nest containing most recent mtgjson update info
+    mtgjson_online_version = content_soup.find('h2').get_text().split()[0] # most recent version number from mtgjson
+    return mtgjson_online_version
+
+
+########################################################################################################################
+
+
+def version_updater(online):
+    """Depending on user input, will update the version file with the build version equal to the online version"""
+    while True:  # loop used to ensure correct user input
+        user_choice = input(
+                    "Build Version does not equal MTGJSON version. Do you wish to record the update? (y/n): ")
+        if user_choice.lower() == 'y':  # i.e. wish to update the stored build value
+            with open('version_check.txt', 'r+') as f:  # open version file
+                f.seek(0)  # go to start of file and write the below
+                new_content = ['MTGJSON_version ' + online + '\n',
+                                       'Build_version ' + online + '\n']  # updating therefore versions will match
+                f.writelines(new_content)  # writes updated file content
+                print('Version file now updated')
+                break
+        elif user_choice.lower() == 'n':  # i.e. just wanted to check if there had been an update or not
+            print('Choice registered, no update will be recorded')
+            break
+        else:
+            print('Invalid choice')  # ensures only either a 'y' or an 'n' input is received
+
+
+########################################################################################################################
+
+
+def main():
+    """Takes version from mtgjson and compares against stored build version. if difference then prompts user"""
+    with open('version_check.txt', 'r+') as f:
+        build_version = f.readlines()[1].split(' ')[1].rstrip()
+    if current_mtgjson_version() == build_version:
+        print('Build and Online Versions Match.')
+    else:
+        version_updater(current_mtgjson_version())
+
+    print('MTGJSON_version = ', current_mtgjson_version())
+    print('Build_version = ', build_version)
+
+
+########################################################################################################################
+
+if __name__ == '__main__':
+    main()  # Checks to see if I need to update the main database or not due to an update of mtgjson data
