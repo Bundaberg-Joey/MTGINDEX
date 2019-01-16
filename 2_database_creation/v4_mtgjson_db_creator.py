dc920fbb263f8d301bf50a6f87ce6633f7d837ab
BundabergJoey <calumhand94@hotmail.com>
2019-01-16 21:44:12 +0000

    Updated script to write output file to a specific database folder.
    Also updated Doc strings in functions

--- 2_database_creation/v4_mtgjson_db_creator.py
+++ 2_database_creation/v4_mtgjson_db_creator.py
@@ -2,16 +2,18 @@ import requests  # required to fetch the web pages containing the JSON formatted
 import json  # required to parse through the JSON data and convert to python iterable values
 import pandas as pd  # required to write the CSV file
 from datetime import datetime  # used in filename versioning
+import os  # used in saving file to other folders
 
 ########################################################################################################################
 
 
 def card_dictionary_writer(set_code):
-
-    """Converts online JSON set info to a list of dictionaries, one dictionary per card in the set.
-    Updated for mtgjson v4."""
-
-    json_set_dictionary = requests.get('https://mtgjson.com/v4/json/'+set_code+'.json')
+    """
+    Converts online MTGJSON info on a passed set to a list of dictionaries, one dictionary per card in the set.
+    :param set_code: 3 or 4 character code used by MTGJSON in naming their online json files, used to complete web URL
+    :return: a list of python dictionaries where each list element is a dictionary and each dictionary is one cards info
+    """
+    json_set_dictionary = requests.get('https://mtgjson.com/json/'+set_code+'.json')
     python_set_dictionary = json.loads(json_set_dictionary.text)  # Convert JSON page to python dict
 
     set_card_dictionaries = []
@@ -19,17 +21,18 @@ def card_dictionary_writer(set_code):
         card_dictionary['mtgjson_set_code'] = set_code  # adds code of the set to the dictionary for later use
         set_card_dictionaries.append(card_dictionary)
 
-    return set_card_dictionaries  # i.e. so return the list of dictionaries, for the set
-
+    return set_card_dictionaries
 
 ########################################################################################################################
 
 
 def card_array_writer(mtgjson_card_dict, mtgjson_attributes):
-
-    """Converts list of dictionaries to a 2D array each sub array contains info for one card from mtgjson.
-    Updated for mtgjson v4."""
-
+    """
+    Converts list of dictionaries to a 2D array each sub array contains info for one card from mtgjson.
+    :param mtgjson_card_dict: Passed dictionary containing one card's information
+    :param mtgjson_attributes: List of dictionary keys expected to be present in this card dictionary
+    :return: a 2D array where each sub array is the data from one card, N/A populated where card doesn't have key value
+    """
     mtgjson_card_array = []  # to be array of all cards originally passed, each sub array = one card
 
     for i in range(0, len(mtgjson_card_dict)):  # dynamically scan the passed dict
@@ -44,7 +47,7 @@ def card_array_writer(mtgjson_card_dict, mtgjson_attributes):
 
         mtgjson_card_array.append(card_attribute_data)  # appends card's array to the main array
 
-    return mtgjson_card_array  # returns 2D array of lists
+    return mtgjson_card_array
 
 
 ########################################################################################################################
@@ -93,13 +96,14 @@ def main():
     set_code_map = pd.read_csv(path_to_set_mapping)  # list of mapped codes
 
     list_of_mtgjson_card_dicts = []  # the list that individual card's dictionary will be added to for parsing in f
-    for mtgjson_card_set in set_code_map['mtgjson_set_code']:  # for every set code in the list of mapped set codes
+    for mtgjson_card_set in set_code_map['mtgjson_set_code']:  # for every set code in the list of mapped set codesset_code_map['mtgjson_set_code']:  # for every set code in the list of mapped set codes
         print('Starting set : ', mtgjson_card_set)  # GUI
         for mtgjson_sing_card_dict in card_dictionary_writer(mtgjson_card_set):  # for every card dict in the set list made
             list_of_mtgjson_card_dicts.append(mtgjson_sing_card_dict) # add to the master list used to create array later
 
     card_attributes = card_attribute_fetcher()  # List of available card attributes from mtgjson
     df = pd.DataFrame(card_array_writer(list_of_mtgjson_card_dicts, card_attributes),columns=card_attributes)
+    os.chdir('../2_database_creation/mtgjson_databases')  # change directory for saving file
     df.to_csv(database_file_name(), index=False)  # save this data frame to appropriately named CSV file.
 
 
