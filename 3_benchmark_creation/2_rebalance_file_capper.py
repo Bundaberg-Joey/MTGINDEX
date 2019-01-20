72428dfdfccfcc82e5dae6df5413dbb7522b20ee
BundabergJoey <calumhand94@hotmail.com>
2019-01-20 09:09:55 +0000

    updated scraper

--- 3_benchmark_creation/2_rebalance_file_capper.py
+++ 3_benchmark_creation/2_rebalance_file_capper.py
@@ -2,20 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 ########################################################################################################################
-# Function : given mkm card web link, function will scrape the web page and return SHINV and UPEUR for the card
 
 
-def mkm_value_scraper(mkm_card_link):
-    page = requests.get(mkm_card_link)  # download page
-    soup = BeautifulSoup(page.content, 'html.parser')  # create beautiful soup object
-    dd_soup = soup.find_all(class_='col-6 col-xl-7')  # filter as required
-    card_shinv = int(dd_soup[3].get_text())  # fetched from Available items field, whole number of cards available
-    card_up = float(dd_soup[5].get_text().split(' ')[0].replace(',', '.')) * 100  # from price trend field, euro cents
-
-    return card_shinv, card_up
+def mkm_value_scraper(mkm_card_url):
+    """
+    Given a card's url, the function parses the webpage's html & returns financial data of the instrument
+    :param mkm_card_url: the link to the card's mkm webpage displaying the required financial info
+    :return: a tuple (number of cards available type=int, price in euros type=float)
+    """
+    page = requests.get(mkm_card_url)  # mkm page for a given card
+    soup = BeautifulSoup(page.content, 'html.parser').find_all(class_='col-6 col-xl-7')  # contains financial info
+    shinv = int(soup[2].get_text())  # shares investable of the card listed on mkm at time of scraping
+    upeur = float(soup[4].get_text().split(' ')[0].replace(',', '.'))  # price trend listed on mkm in euros
+    return shinv, upeur
 
 
 ########################################################################################################################
-print(mkm_value_scraper('https://www.cardmarket.com/en/Magic/Products/Singles/Tenth+Edition/True-Believer'))
-
-# TODO : have this run through all cards in a given file and append results as a column + calcd caps then sort 
+print(mkm_value_scraper('https://www.cardmarket.com/en/Magic/Products/Singles/Aether+Revolt/Restoration-Specialist'))
