e0af0403d9bfe697d5e72d1f986564febe3b68ee
BundabergJoey <calumhand94@hotmail.com>
2019-01-20 20:20:38 +0000

    can now filter based on standard equality

--- 3_benchmark_creation/1_rebalance_file_maker.py
+++ 3_benchmark_creation/1_rebalance_file_maker.py
@@ -5,11 +5,18 @@
 # TODO 5) the fully filtered pandas db is then written to a csv file with the same name as criteria file
 ########################################################################################################################
 import pandas as pd
+import json
 
 
 df_criteria = pd.read_json('../3_benchmark_creation/benchmark_criteria_files/2.json')  # criteria file
-mandatory_cols = ['uuid', 'mkm_url'] + df_criteria['property'].tolist()  # list of required headers for rebalance db
+mandatory_cols = ['uuid', 'mkm_url'] + df_criteria['attribute'].tolist()  # list of required headers for rebalance db
 
-df_database = pd.read_csv('../2_database_creation/Card_Databases/Card_Database_421_20190120_0949.csv')[mandatory_cols]  # main card db
-print(df_database.columns)
+df = pd.read_csv('../2_database_creation/Card_Databases/Card_Database_421_20190120_0949.csv')[mandatory_cols]  # main card db
 
+
+for i in range(len(df_criteria.index)):  # for i in range(number of rows in the criteria file)
+    crt_attr, crt_op, crt_val = df_criteria.loc[i]  # unpack each assignment into three values
+    if crt_val.isnumeric():  # if the criteria value is a number but read as string
+        crt_val = int(crt_val)  # convert to integer if needed (i.e. for ConvertedManaCost)
+    df = df[df[crt_attr] == crt_val]  # df where df[row told to look at] == value told to equal
+    print(df.shape)
