3f51b892a7922cca820764f0e4fee148a0fbd687
BundabergJoey <calumhand94@hotmail.com>
2019-01-21 22:01:44 +0000

    file now runs dynamically for any number of given bechmark criteria files. Need to still improve on the filtering functionality though as still a bit bare bones at the moment and not actually sure if working properly.

--- 3_benchmark_creation/1_rebalance_file_maker.py
+++ 3_benchmark_creation/1_rebalance_file_maker.py
@@ -19,40 +19,49 @@ def criteria_enforcer(card_df, criteria):
         card_df = card_df[card_df[crt_attribute] == crt_value]  # filter where attribute  ==   value
         return card_df
     elif crt_operator == '>':
-        card_df = card_df[card_df[crt_attribute] > crt_value]  # filter where attribute  ==   value
+        card_df = card_df[card_df[crt_attribute] > crt_value]  # filter where attribute  >   value
         return card_df
     elif crt_operator == '>=':
-        card_df = card_df[card_df[crt_attribute] >= crt_value]  # filter where attribute  ==   value
+        card_df = card_df[card_df[crt_attribute] >= crt_value]  # filter where attribute  >=   value
         return card_df
     elif crt_operator == '<':
-        card_df = card_df[card_df[crt_attribute] < crt_value]  # filter where attribute  ==   value
+        card_df = card_df[card_df[crt_attribute] < crt_value]  # filter where attribute  <   value
         return card_df
     elif crt_operator == '<=':
-        card_df = card_df[card_df[crt_attribute] <= crt_value]  # filter where attribute  ==   value
+        card_df = card_df[card_df[crt_attribute] <= crt_value]  # filter where attribute  <=   value
         return card_df
     elif crt_operator == '!=':
-        card_df = card_df[card_df[crt_attribute] != crt_value]  # filter where attribute  ==   value
+        card_df = card_df[card_df[crt_attribute] != crt_value]  # filter where attribute  !=   value
         return card_df
 
 
 ########################################################################################################################
-os.chdir('../3_benchmark_creation/benchmark_criteria_files')  # folder of criteria files, 1 file per benchmark
-criteria_files = [i for i in os.listdir(os.getcwd())]  # list of criteria files
+criteria_files_path = '../3_benchmark_creation/benchmark_criteria_files/'  # relative path of script to criteria files
+card_database_path = '../../2_database_creation/Card_Databases/'  # relative path to main db once directory changed
+benchmark_database_path = '../benchmark_rebalance_files'  # relative path to directory where the benchmarks are saved
+looping_criteria_path = '../benchmark_criteria_files/'  # because I'm folders change frequently need to restate for loop
+mandatory_db_cols = ['uuid', 'mkm_url']  # columns always required from the parent db in child benchmark
 
-for crt_file in criteria_files:  # for every criteria file in the folder
-    df_criteria = pd.read_json(crt_file)  # read the file into a json format
-    mandatory_cols = ['uuid', 'mkm_url'] + df_criteria['attribute'].tolist()  # list of required headers for rebalance db
 
-    df_database = pd.read_csv('../../2_database_creation/Card_Databases/Card_Database_421_20190120_0949.csv')[mandatory_cols]  # main card db
+os.chdir(criteria_files_path)  # folder of criteria files, 1 file per benchmark
+criteria_files = [i for i in os.listdir(os.getcwd())]  # list of criteria files to be actioned in this script
+database_name = [i for i in os.listdir(card_database_path) if '.csv' in i][0]  # filename of most recent card database
 
-    for i in range(len(df_criteria.index)):  # for i in range(number of rows in the criteria file)
-        df_database = criteria_enforcer(df_database, df_criteria.loc[i])  # pass dataframe and ith fow of criteria file
-        if df_database.size == 0:  # check to see if resultant database is empty
-            print('The filtered database is empty')  # GUI
 
-    os.chdir('../benchmark_rebalance_files')  # change to where the rebalance file will be saved
-    df_database.to_csv('thingy.csv', index=False)  # write the file to a name
+for crt_file in criteria_files:  # for every criteria file in the folder (i.e. for every benchmark to be created)
+    os.chdir(looping_criteria_path)
+    print(f'parsing "{crt_file}"')  # GUI
+    df_criteria = pd.read_json(crt_file)  # read the criteria file into a json format
+    mandatory_cols =  mandatory_db_cols + df_criteria[df_criteria.columns[0]].tolist()  # cols needed for benchmark file
 
-# Can crudely do multiple filterings given operators.
-# TODO 3) Write each totally filtered df to a csv with appropriate name
-# TODO 4) needs to take most recent card database file as input also as currently hardcoded for just the one
+    benchmark_df = pd.read_csv(card_database_path + database_name)[mandatory_cols]  # load card db with required cols
+    for i in range(len(df_criteria.index)):  # for i in range(number of rows in the criteria file) i.e. for every row
+        print(f'    parsing criteria {i+1}')  # GUI
+        benchmark_df = criteria_enforcer(benchmark_df, df_criteria.loc[i])  # pass df and 'i'th fow of criteria file
+
+    os.chdir(benchmark_database_path)  # change to where the rebalance file will be saved
+    benchmark_file_name = crt_file.split('.')[0]+'.csv'  # save name is the same as the criteria file but .csv extension
+    benchmark_df.to_csv(benchmark_file_name, index=False)  # write the benchmark database to a dynamic filename
+
+
+# TODO : Test on larger scale for multiple filterings to ensure benchmark creation is consistent and reliable
