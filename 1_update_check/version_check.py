09930b739330783c4466990d1d68aeb80cb61372
Calum Hand <crh53@bath.ac.uk>
2019-01-24 22:02:54 +0000

    updated doccumentation.

--- 1_update_check/version_check.py
+++ 1_update_check/version_check.py
@@ -3,10 +3,14 @@ import json
 ########################################################################################################################
 
 
-def current_mtgjson_version():
-    """Fetches current version number from mtgjson online and returns value as a string"""
-    page = requests.get('https://mtgjson.com/json/version.json')  # json file containing version number
-    mtgjson_online_version = json.loads(page.content)['version']  # parsed json containing
+def current_mtgjson_version(json_url):
+    """
+    Fetches current version number from mtgjson online and returns value as a string
+    :param json_url: URL to the json page where mtgjson display the current online version number
+    :return: the current online mtgjson version as a string
+    """
+    page = requests.get(json_url)  # online json file containing version number of mtgjson online
+    mtgjson_online_version = json.loads(page.content)['version']  # parse json to retrieve the online version number
     return mtgjson_online_version
 
 
@@ -16,16 +20,16 @@ def current_mtgjson_version():
 def main():
     """Takes version from mtgjson and compares against stored build version. if difference then prompts user"""
     with open('build_version.json', 'r') as f:
-        build_version = json.loads(f.read())['Build']
+        build_version = json.loads(f.read())['Build']  # loads locally stored build number for comparison
 
-    mtgjson_version = current_mtgjson_version()
-    print(f'MTGJSON_version = {mtgjson_version}')
-    print(f'Build_version = {build_version}')
+    mtgjson_version = current_mtgjson_version('https://mtgjson.com/json/version.json')
+    print(f'MTGJSON_version = {mtgjson_version}')  # GUI
+    print(f'Build_version = {build_version}')  # GUI
 
     if  mtgjson_version == build_version:
-        print('Build and Online Versions Match.')
+        print('Build and Online Versions Match.')  # no need to recreate local database
     else:
-        print('Build Version does not equal MTGJSON version')
+        print('Build Version does not equal MTGJSON version')  # should update local db (build version updates during)
 
 
 ########################################################################################################################
