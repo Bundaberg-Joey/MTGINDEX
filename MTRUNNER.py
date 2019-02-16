ba0382e2018298498ba709fc80e05eaadc176af8
BundabergJoey <calumhand94@hotmail.com>
2019-02-16 23:25:23 +0000

    combined all working scripts into singular file which can be run from standard terminal

---
+++ MTRUNNER.py
@@ -0,0 +1,31 @@
+import os
+from MTTOOLS import Version_check
+from MTTOOLS import Card_Database_Creator
+from MTTOOLS import Mkm_Data_Scraper
+
+########################################################################################################################
+
+
+def main():
+    """
+    MTGINDEX program file. Checks versions match and builds MTCARD database if versions do not match. Script then
+    continues to scrape MKM in order to generate the daily MTDATA file
+    :return: No value returned, however MTDATA file generated daily and MTCARD file generated when required
+    """
+
+    os.chdir('../MTGINDEX/MTTOOLS')  # change directory to allow MTTOOLS to function
+    mtcard_versions_match = Version_check.main()  # Function checks if stored and online mtgjson versions match, bool
+
+    if mtcard_versions_match == True:
+        print('MTGJSON and Build versions match, MTCARD rebuild not required')  # GUI
+    elif mtcard_versions_match == False:
+        print('MTGJSON and Build versions out of sync, MTCARD rebuild required')  # GUI
+        Card_Database_Creator.main()  # Generate a new MTCARD database file, only needed when mtgjson updates ~ monthly
+    print('Generating MTDATA')  # GUI
+    Mkm_Data_Scraper.main()  # Scrape mkm to get the daily UPEUR  and SHINV for listed cards, file saved to MTDATA
+
+
+########################################################################################################################
+
+if __name__ == '__main__':
+    main()
