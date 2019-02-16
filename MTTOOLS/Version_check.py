28b111d6aed37d75eb83fa31b247c33af3c2a625
BundabergJoey <calumhand94@hotmail.com>
2019-02-16 22:48:05 +0000

    trimmed down clutter further in the file

--- MTTOOLS/Version_check.py
+++ MTTOOLS/Version_check.py
@@ -3,29 +3,17 @@ import json
 ########################################################################################################################
 
 
-def current_mtgjson_version(json_url):
-    """
-    Fetches current version number from mtgjson online and returns value as a string
-    :param json_url: URL to the json page where mtgjson display the current online version number
-    :return: the current online mtgjson version as a string
-    """
-    page = requests.get(json_url)  # online json file containing version number of mtgjson online
-    mtgjson_online_version = page.json()['version']  # parse json to retrieve the online version number
-    return mtgjson_online_version
-
-
-########################################################################################################################
-
-
 def main():
-    """Takes version from mtgjson and compares against stored build version. Returns True if the versions match,
-    otherwise it returns false
-    :return : Boolean, True if build version and mtgjson version match
+    """ Loads locally stored build version and online mtgjson versions and compares them.
+    :return : Boolean, True if build version and mtgjson version match, False if they do not match
     """
     with open('../../MTGINDEX/MTREFS/build_version.json', 'r') as f:
         build_version = json.loads(f.read())['Build']  # loads locally stored build number for comparison
-    mtgjson_version = current_mtgjson_version('https://mtgjson.com/json/version.json')  # online version
-    return True if mtgjson_version == build_version else False
+
+    page = requests.get('https://mtgjson.com/json/version.json')  # json file hosted by mtgjson listing version number
+    mtgjson_online_version = page.json()['version']  # parse json to retrieve the online version number
+    
+    return True if mtgjson_online_version == build_version else False
 
 
 ########################################################################################################################
