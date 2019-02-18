0a44b16e9a653fcd56da30b46329641cf9f1a94e
BundabergJoey <calumhand94@hotmail.com>
2019-02-18 21:10:14 +0000

    Updating print statements

--- MTRUNNER.py
+++ MTRUNNER.py
@@ -17,13 +17,14 @@ def main():
     mtcard_versions_match = Version_check.main()  # Function checks if stored and online mtgjson versions match, bool
 
     if mtcard_versions_match == True:
-        print('MTGJSON and Build versions match, MTCARD rebuild not required')  # GUI
+        print(' >> MTGJSON and Build versions match, MTCARD rebuild not required')  # GUI
     elif mtcard_versions_match == False:
-        print('MTGJSON and Build versions out of sync, MTCARD rebuild required')  # GUI
+        print(' >> MTGJSON and Build versions out of sync, MTCARD rebuild required')  # GUI
         Card_Database_Creator.main()  # Generate a new MTCARD database file, only needed when mtgjson updates ~ monthly
-    print('Generating MTDATA')  # GUI
+        print('  >>> MTCARD File Built')  # GUI
+    print(' >> Generating MTDATA')  # GUI
     Mkm_Data_Scraper.main()  # Scrape mkm to get the daily UPEUR  and SHINV for listed cards, file saved to MTDATA
-
+    print('  >>> MTDATA File Built')  # GUI
 
 ########################################################################################################################
 
