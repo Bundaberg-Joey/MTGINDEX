5b0f7c71be5b3c7a6bf451ed8b78edf67f6581f1
BundabergJoey <calumhand94@hotmail.com>
2020-04-10 22:51:23 +0100

    updating main script with OOP changes

--- main.py
+++ main.py
@@ -1,18 +1,52 @@
-import yaml
-import os
-
-from mtgindex.VersionControl import VersionController
-from mtgindex.DatabaseAssembly import AssembleSQL
+from mtgindex import utilities
+from mtgindex.build import VersionController, AssembleSQL
+from mtgindex.Allocation import ConstituentHandler
+from mtgindex.Finance import BenchmarkHandler
 
 if __name__ == '__main__':
-    with open('MTREF/config.yaml') as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
 
+    config = utilities.load_yaml('reference/config.yaml')
+
+    mtgjson_loc = config['mtgjson_local_loc']
+    mtcard_loc = config['mtcard_loc']
+    mtbenchmark_loc = config['mtbenchmark_loc']
+
+    print('version control')
     vc = VersionController()
-    vc.fetch_current(config['local_version_path'])
-    vc.fetch_queried(config['remote_version_url'])
+    vc.fetch_current(location=config['local_version_loc'])
+    vc.fetch_queried(location=config['remote_version_url'])
 
-    if vc.compare_versions() == False:
-        db = AssembleSQL.retrieve_data(config['database_url'])
-        save_name = os.path.join(config['local_database_location'], '_safe_to_delete.sqlite')
-        db.build(save_name)
+    if vc.compare_versions() is False:
+        print('retrieving database')
+        db = AssembleSQL.download_data(location=config['database_url'])
+        print('building database')
+        db.build(destination=mtcard_loc)
+
+    database_locations = [mtgjson_loc, mtcard_loc, mtbenchmark_loc]
+    mtgjson_conn, mtcard_conn, mtbenchmark_conn = utilities.establish_connection(database_locations)
+
+    benchmark_queries = utilities.load_yaml(config['mtqueries_loc'])
+    print('initialising handler')
+    ch = ConstituentHandler(cons_origin=mtgjson_conn, cons_dest=mtcard_conn)
+    bh = BenchmarkHandler(constituent_conn=mtcard_conn, benchmark_conn=mtbenchmark_conn)
+    price_date = vc.format_pricedate(current='%Y-%m-%d', out='%Y-%m-%d')
+
+    for benchmark in benchmark_queries:
+        print('benchmark')
+        query = benchmark_queries[benchmark]
+        constituents = ch.select_cons(benchmark_query=query)
+
+        if len(constituents) > 0:
+            print('\t', 'Saving', benchmark)
+            ch.save_cons(table=benchmark, constituents=constituents)
+            # if table in benchmark level schema then calculate / append price
+            # if not in benchmark table, create benchmark with new value
+
+        else:
+            print('\t', 'Dropping', benchmark)
+            ch.drop_cons_table(table=benchmark)
+            # if in benchmark table but not cons database then add nan value to benchmark for date
+
+# TODO 1: Update so that price information is also retrieved with constituents. (don't break to much).
+# TODO 2: Update index level with constituents.
+# TODO 3: Store constituent level information.
