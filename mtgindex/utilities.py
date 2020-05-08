91cc6929fca23a0acbcb9bd2a793e419ecb45efb
Bundaber-Joey <calumhand94@hotmail.com>
2020-05-08 15:07:55 +0100

    Added user warning to `utilities.establish_connection`and updated to use `*args`

--- mtgindex/utilities.py
+++ mtgindex/utilities.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import warnings
 
 import yaml
 
@@ -22,12 +23,13 @@ def load_yaml(path):
     return content
 
 
-def establish_connection(db_paths):
-    """Allows for establishing connections to multiple existing, local, sqlite databases.
+def establish_connection(*args):
+    """Allows for establishing connections to sqlite databases.
+    If the database does not exist when making the connection then a new database will be created.
 
     Parameters
     ----------
-    db_paths : list[str]
+    args : iterable of strings.
         Paths to the databases.
 
     Returns
@@ -35,5 +37,10 @@ def establish_connection(db_paths):
     connections : tuple[sqlite connection]
         Each entry is the connection to an sqlite database.
     """
-    connections = (sqlite3.connect(db) for db in db_paths if os.path.exists(db))
+    connections = []
+    for db_path in args:
+        if not os.path.exists(db_path):
+            warnings.warn('sqlite database does not exist, creating db to enable connection', UserWarning)
+        conn = sqlite3.connect(db_path)
+        connections.append(conn)
     return connections
