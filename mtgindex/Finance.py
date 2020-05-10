01434c08a8c4fd60b1c3b3a65e40a0fa38cbdc16
Bundaber-Joey <calumhand94@hotmail.com>
2020-05-10 09:48:32 +0100

    Updated `price_weighted_index` calculation in line with project specs.

--- mtgindex/Finance.py
+++ mtgindex/Finance.py
@@ -1,13 +1,24 @@
-def price_weighted_index(prior_level, constituent_prices, **kwargs):
-    """Calculates a price weighted index level based on returns.
-    All constituents are assumed to hold an equally weighted contribution to the index (sum of weights is 1).
-    level_t = level_{t-1} \times (1 + \sum_{i=0} c_i \cdot w_i)
+def price_weighted_index(prices, weights, prior_level):
+    """Calculates the price weighted index level of a benchmark.
+    $$level_t = level_{t-1} \times (1 + \sum_{i=0} c_i \cdot w_i)$$
+
+    Parameters
+    ----------
+    prices : list, shape(<num_constituents>)
+        Price evaluations of constituents.
+
+    weights : list, shape(<num_constituents>)
+        Price based weightings of constituents within the benchmark
+
+    prior_level : float
+        Previously calculated index value for the benchmark.
 
     Returns
     -------
-    None
+    new_level : float
+        Price weighted index value.
     """
-    # weighting = 1/len(constituent_prices)
-    # level = prior_level * (1 + sum(constituent_prices) * weighting)
-    # return level
-    return NotImplementedError
+    assert len(weights) == len(prices), F'Prices {len(prices)} and Weights {len(weights)} passed are not equal length.'
+    weighted_prices = sum((p*w for p, w in zip(prices, weights)))
+    new_level = prior_level * (1 + weighted_prices)
+    return new_level
