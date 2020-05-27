58778f5bf8a01d6571a4de0b63313378d7c817f7
Bundaber-Joey <calumhand94@hotmail.com>
2020-05-27 21:45:06 +0100

    rough working copy up and running.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sqlite3
 from mtgindex import utilities
 from mtgindex.build import VersionControl, AssembleSQL
 from mtgindex.benchmark import PriceWeightedBenchmark
+from mtgindex.database import MtgIndex
 
 if __name__ == '__main__':
 
@@ -15,16 +16,17 @@ if __name__ == '__main__':
     vc.fetch_queried(location='https://mtgjson.com/json/version.json')
 
     if vc.compare_versions() is False:
-        print('retrieving database')
         db = AssembleSQL.download_data('https://www.mtgjson.com/files/AllPrintings.sqlite')
-        print('building database')
         db.build(destination=mtgjson_loc)
 
     index_queries = utilities.load_yaml('reference/mtqueries.yml')
-    price_date = vc.format_pricedate(current='%Y-%m-%d', out='%Y-%m-%d')
+    price_date = '2020-05-24'  #vc.format_pricedate(current='%Y-%m-%d', out='%Y-%m-%d')
+    # hard code date for initial testing
 
     conn = sqlite3.connect(mtgjson_loc)
     curr = conn.cursor()
+
+    mtg = MtgIndex(mtgindex_loc)  # create object to interact with main database
 
     for index_name in index_queries:
         print(index_name)
@@ -33,6 +35,20 @@ if __name__ == '__main__':
         benchmark.apply_criteria(curr)
         benchmark.evaluate_constituents(curr)
 
-    # TODO 1: Test that the PriceWeightedBenchmark class works like it's meant to!!!
-    # TODO 2: Once fully integrated, create yaml file which tracks dates which have been assessed
-    # TODO 3: develop a function which can see which dates have not been assessed from MTGJSON database so can iterate through them
+        if mtg.table_exist(index_name):
+            print('\t', 'updating mtgindex table')
+            benchmark.get_prior_index(mtgindex_curr=mtg.curr)  # retrieve prior index value for calculation
+            benchmark.weight_constituents()  # caluculate index level
+            benchmark.calculate_index_level()
+            mtg.update_benchmark_table(benchmark)
+        else:
+            mtg.add_benchmark(benchmark)
+            print('\t', 'creating mtgindex table')
+            benchmark.get_prior_index(mtgindex_curr=mtg.curr)  # retrieve prior index value for calculation
+            benchmark.index_value = 1000.0
+            mtg.update_benchmark_table(benchmark)
+
+    # TODO : Set the index value to be 1000.0 using the benchmark class rather than through hard coding
+    # TODO : Have tested the benchmark and can create index values for 2020-05-24 but need to see about how to update the values for new dates
+    # TODO 2: For each benchmark, see which dates exist in mtgindex and then evaluate for any FUTURE price dates present which are not in mtgindex
+    # FUTURE prices need to be used to avoid adding in new index values which are prior information which just fucks the whole thing!!
